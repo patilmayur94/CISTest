5284938353024855c7d63584bab68f70c9eaf1bf
mayur patil <patilmayur94@gmail.com>
2018-12-04 16:07:10 +0530

    Add files via upload

---
+++ Login App/src/com/gslab/LoginValidate.java
@@ -0,0 +1,52 @@
+package com.gslab;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginValidate
+ */
+public class LoginValidate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginValidate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.getWriter().append("Hello "+request.getHeader("iv-user")+" ,\n");
+		response.getWriter().append("Title "+request.getHeader("title")+" ,\n");
+		response.getWriter().append("Mail "+request.getHeader("email"));
+		Enumeration<String> headers=request.getHeaderNames();
+		while(headers.hasMoreElements()) {
+			String header=headers.nextElement();
+			System.out.print(header+"  :   ");
+			System.out.println(request.getHeader(header));
+			//request.getHeader()
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
